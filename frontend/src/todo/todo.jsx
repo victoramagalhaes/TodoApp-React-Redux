0e5bbf232a34441b5170450c2ff609cf91f39135
Victor Magalhães <contatovictormagalhaes@hotmail.com>
2018-05-14 04:44:46 -0300

    Adicionando todoForm e todoList

--- frontend/src/todo/todo.jsx
+++ frontend/src/todo/todo.jsx
@@ -1,12 +1,17 @@
 import React, { Component } from 'react'
 import PageHeader from '../template/pageHeader.jsx'
+import TodoForm from './todoForm.jsx'
+import TodoList from './todoList.jsx'
 
 export default class Todo extends Component {
     render(){
         return(
             <div>
                 <PageHeader name="Tarefas" small="Cadastro"></PageHeader>
+                <TodoForm />
+                <TodoList />
             </div>
+                
         )
     }
 
