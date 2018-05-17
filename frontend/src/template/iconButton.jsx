30d4bad8eaf74211f4848326f62e562742c8a9fe
Victor Magalhães <contatovictormagalhaes@hotmail.com>
2018-05-17 04:10:03 -0300

    Insert IF

--- frontend/src/template/iconButton.jsx
+++ frontend/src/template/iconButton.jsx
@@ -1,15 +1,11 @@
 import React from 'react'
+import If from './if.jsx'
 
-export default props =>{
-    if(props.hide){
-        return null
-    }
-    else{
-        return (
+export default props =>(
+            <If test={!props.hide}>
             <button className={'btn btn-'+ props.style}
                 onclick={props.onClick}>
                 <i className={'fa fa-'+ props.icon}></i>
             </button>
-        )
-    }
-}
+            </If>
+)
