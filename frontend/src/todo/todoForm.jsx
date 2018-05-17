68960d9df25022533671fdc7b2674a19c51fcd98
Victor Magalhães <contatovictormagalhaes@hotmail.com>
2018-05-17 00:10:09 -0300

    Insert Grid and Integrating iconButton

--- frontend/src/todo/todoForm.jsx
+++ frontend/src/todo/todoForm.jsx
@@ -1,16 +1,17 @@
 import React from 'react'
+import Grid from '../template/grid.jsx'
+import IconButton from '../template/iconButton.jsx'
 
 export default props =>(
     <div role="form" className="todoForm">
         <div className="row">
-            <div className="col-xs-12 col-sm-9 col-md-10">
+            <Grid cols="12 9 10">
                 <input id="description" className="form-control" placeholder="Adicione uma tarefa"></input>
-        </div>
-        <div className="col-xs-12 col-sm-3 col-md-2">
-            <button className="btn btn-primary">
-                <i className="fa fa-plus"></i>
-            </button>
-        </div>
+            </Grid>
+            
+            <Grid cols="12 3 2">
+                <IconButton style='primary' icon='plus'></IconButton>
+            </Grid>
         </div>
     </div>
 )
