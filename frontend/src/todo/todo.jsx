1fa144875d12382a7f17d5ad63db6f047ffa91f7
Victor Magalhães <contatovictormagalhaes@hotmail.com>
2018-05-19 23:24:17 -0300

    Making posts works

--- frontend/src/todo/todo.jsx
+++ frontend/src/todo/todo.jsx
@@ -12,8 +12,14 @@ export default class Todo extends Component {
         this.state = { description: "", list: [] }
         this.handleChange = this.handleChange.bind(this)
         this.handleAdd = this.handleAdd.bind(this)
+        this.refresh()
     }
     
+    refresh(){
+        axios.get(`${URL}?sort=-createdAt`)
+        .then(resp => this.setState({...this.state, description: '', list: resp.data}))
+    }
+
     handleChange(e){
         this.setState({description: e.target.value})
     }
@@ -21,7 +27,7 @@ export default class Todo extends Component {
     handleAdd(){
         const description = this.state.description
         axios.post(URL, { description })
-        .then(resp => console.log("Funcionou!"))
+        .then(resp => this.refresh())
     }
     
     render(){
@@ -31,7 +37,7 @@ export default class Todo extends Component {
                 <TodoForm description={this.state.description}
                 handleChange={this.handleChange}
                 handleAdd={this.handleAdd}/>
-                <TodoList />
+                <TodoList list={this.state.list} />
             </div>
                 
         )
