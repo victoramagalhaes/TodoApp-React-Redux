2e93c2c0e4c88353b0e6dad114643b95e8d49565
Victor Magalhães <contatovictormagalhaes@hotmail.com>
2018-05-19 21:30:08 -0300

    Implement todo post v1

--- frontend/src/todo/todo.jsx
+++ frontend/src/todo/todo.jsx
@@ -2,6 +2,9 @@ import React, { Component } from 'react'
 import PageHeader from '../template/pageHeader.jsx'
 import TodoForm from './todoForm.jsx'
 import TodoList from './todoList.jsx'
+import axios from 'axios'
+
+const URL = 'http://localhost:3003/api/todos'
 
 export default class Todo extends Component {
     constructor(props){
@@ -16,7 +19,9 @@ export default class Todo extends Component {
     }
 
     handleAdd(){
-        console.log(this.state.description)
+        const description = this.state.description
+        axios.post(URL, { description })
+        .then(resp => console.log("Funcionou!"))
     }
     
     render(){
