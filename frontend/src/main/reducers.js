19dbf9022a3b643d8a2c897c8d3c981694c470fb
Victor Magalhães <contatovictormagalhaes@hotmail.com>
2018-06-02 17:10:16 -0300

    Implementando a adição e atualização da lista em redux

--- frontend/src/main/reducers.js
+++ frontend/src/main/reducers.js
@@ -1,22 +1,8 @@
 import { combineReducers } from "redux"
+import todoReducer from '../todo/todoReducer.js'
 
 const rootReducer = combineReducers({
-    todo: () => ({
-        description: "Ler Livro",
-        list: [{
-            _id: 1,
-            description: "Pagar fatura do cartão",
-            done:true
-        }, {
-            _id: 2,
-            description: "Reunião com a Equipe",
-            done:false
-        }, {
-            _id: 3,
-            description: "Consulta médicas na terça depois do almoço",
-            done: false
-        }]
-    })
+    todo: todoReducer
 })
 
 export default rootReducer
--- frontend/src/todo/todoForm.jsx
+++ frontend/src/todo/todoForm.jsx
@@ -1,41 +1,56 @@
-import React from 'react'
+import React, {Component} from 'react'
+import { connect } from 'react-redux'
+import { bindActionCreators } from 'redux'
+
 import Grid from '../template/grid.jsx'
 import IconButton from '../template/iconButton.jsx'
-import { connect } from 'react-redux'
+import { add, changeDescription, search } from './todoAction.js'
 
-const TodoForm = props => {
-    const EnterClick = (e) => {
+class TodoForm extends Component {
+    constructor(props){
+        super(props)
+        this.EnterClick = this.EnterClick.bind(this)
+    }
+    componentWillMount(){
+        this.props.search()
+    }
+    EnterClick(e){
+        const { add, search, description } = this.props
         if (e.key === "Enter"){
-            e.shiftKey ? props.handleSearch() : props.handleAdd()
+            e.shiftKey ? search() : add(description)
         }
         else if (e.key === "Escape"){
             props.handleClear()
         }
     }
-    
-    return (
-        <div role="form" className="todoForm">
+    render(){
+        const { add, search, description } = this.props
+        return (
+            <div role="form" className="todoForm">
             <div className="row">
                 <Grid cols="12 9 10">
                     <input id="description" className="form-control" 
                     placeholder="Adicione uma tarefa"
-                    onKeyUp={EnterClick}
-                    onChange={props.handleChange}
-                    value={props.description}></input>
+                    onKeyUp={this.EnterClick}
+                    onChange={this.props.changeDescription}
+                    value={this.description}></input>
                 </Grid>
                 
                 <Grid cols="12 3 2">
                     <IconButton style='primary' icon='plus' 
-                    onClick={props.handleAdd}></IconButton>
+                    onClick={() => add(description)}></IconButton>
                     <IconButton style='info' icon='search' 
-                    onClick={props.handleSearch}></IconButton>
+                    onClick={() => search()}></IconButton>
                     <IconButton style='default' icon='close' 
-                    onClick={props.handleClear}></IconButton>
+                    onClick={this.handleClear}></IconButton>
                 </Grid>
             </div>
         </div>
-    )
+        )
+    }
 }
 
 const MapStateToProps = state => ({description: state.todo.description})
-export default connect(MapStateToProps)(TodoForm)
+const mapDispatchToProps = dispatch => 
+    bindActionCreators({ add, changeDescription, search }, dispatch)
+export default connect(MapStateToProps,mapDispatchToProps)(TodoForm)
